b520c16d0be7c61d6ef4567027a73746c8dfed14
Andres RH <andres.rossi@ueda.edu.co>
2023-10-06 10:49:37 -0500

    creacion de funciones y uso de Tailwind

---
+++ components/Button.tsx
@@ -0,0 +1,7 @@
+const Button = () => {
+    return (
+       <button className='bg-blue-300 text-white rounded-lg'>Has click acá</button> 
+    )
+}
+
+export { Button };
---
+++ pages/andres.tsx
@@ -0,0 +1,10 @@
+const AndresPage = () => {
+    return (
+    
+    <div>Hola soy la pagina de Andres</div>
+    
+    )
+
+};
+
+export default AndresPage
